02b4ba90d8a42f600651211f23a75ad1a10a1486
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-08-04 16:58:59 +0100

    feat: configure goerli testnet for hardhat and install dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY!;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.15",
+  networks: {
+    goerli: {
+      url: `https://rpc.goerli.mudit.blog/`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
